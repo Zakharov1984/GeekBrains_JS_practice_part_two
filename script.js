f906ad1d4e1e58f7feb0b0a3301b1d97e056e4e0
Zakharov <zakharov.products@gmail.com>
2021-09-23 16:54:07 +0300

    finished_work_for_the_second_lesson

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
-let productItems = [
-    {title: 'картина', price: 300},
-    {title: 'рамка', price: 50},
-    {title: 'фотография', price: 100},
-    {title: 'шаблон', price: 150}
-];
+class ProductList{
+    constructor(container = '.products-block') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render(); // вывод товаров на страницу.
+    }
+    _fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'картина', price: 300},
+            {id: 2, title: 'рамка', price: 50},
+            {id: 3, title: 'фотография', price: 100},
+            {id: 4, title: 'шаблон', price: 150}
+        ];
+    }
+    render() {
+        let productsBlock = document.querySelector('.products-block');
+        for (let product of this.goods) {
+            let item = new ProductItem(product);
+            productsBlock.insertAdjacentHTML('beforeend', item.renderItem());
+        }
 
-/**
- * Функция возвращает верстку одного товара
- * @param {object} item 
- * @returns {string}
- */
-
-function renderItem(item) {
-    return  `<div class="product-item">
-                <image class="product-item__image" src="image.jpg"></image>
-                <h3 class="product-item__title">${item.title}</h3>
-                <p class="product-item__price">${item.price}</p>
-                <button class="product-item__book">Заказать</button>
-            </div>`
+    }
+    calcTotalSum() {
+        let totalSum = 0;
+        for (let product of this.goods) {
+            totalSum = totalSum + product.price;        
+        }
+        console.log(totalSum);
+    }
 }
 
-
-/**
- * Функция генерирует верстку всех товаров и добавляет в блок products-block
- * @param {array} productItems 
- */
-function renderAllItems(productItems) {
-    let productsBlock = document.querySelector('.products-block');
-    let ListItem = productItems.map(item => renderItem(item));
-    ListItem = ListItem.join('');
-    productsBlock.insertAdjacentHTML('afterbegin', ListItem);
+class ProductItem {
+    constructor(product, img = 'image.jpg') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+        
+    }
+    renderItem() {
+        return  `<div class="product-item">
+                    <image class="product-item__image" src="${this.img}"></image>
+                    <h3 class="product-item__title">${this.title}</h3>
+                    <p class="product-item__price">${this.price}</p>
+                    <button class="product-item__book">Заказать</button>
+                </div>`
+    }
 }
 
+class basketGoods{
+    addedProduct() {
 
-renderAllItems(productItems);
+    }
+    deleteProduct() {
+
+    }
+    
+}
+
+class elementBasketGoods{
+
+}
+
+let list = new ProductList();
+console.log(list);
